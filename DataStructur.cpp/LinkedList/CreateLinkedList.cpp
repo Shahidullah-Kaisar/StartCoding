10c87e7141f97b1cc260f45bb6122aae0c293898
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2026-02-03 23:44:30 +0600

    add push_back method

--- DataStructur.cpp/LinkedList/CreateLinkedList.cpp
+++ DataStructur.cpp/LinkedList/CreateLinkedList.cpp
@@ -32,6 +32,18 @@ public:
         }
     }
 
+    void push_back(int val){
+        Node* newNode = new Node(val);
+
+        if(head == NULL){
+            head = tail = newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+    }
+
     void printList(){
         Node* temp = head;
         while(temp != NULL){
@@ -48,6 +60,8 @@ int main(){
     ll.push_front(2);
     ll.push_front(3);
     ll.push_front(4);
+
+    ll.push_back(5);
 
     ll.printList();
 
